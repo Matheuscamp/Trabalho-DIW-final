c27e76a082904db41052fd5e6f6304ed34c37b15
Matheus Campos <103076645+Matheuscamp@users.noreply.github.com>
2023-06-09 13:35:47 -0300

    Update script_detalhes.js
    script detalhes update

--- script_detalhes.js
+++ script_detalhes.js
@@ -1,19 +1,17 @@
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get("id");
+const urlParametros = new URLSearchParams(window.location.search);
+const produtoID = urlParametros.get("id");
 
-fetch(`http://diwserver.vps.webdock.cloud:8765/products/${productId}`)
+fetch(`https://diwserver.vps.webdock.cloud/products/${produtoID}`)
   .then((response) => response.json())
   .then((data) => {
     const detalhesProduto = `
-  
-              <h3>${data.title}</h3>
-          
-              <img src="${data.image}" class="Img-card" alt="Product Image">
-              <h2>Price:<span class="cor_do_preco"> $${data.price}</h2></span>
-              <p>Description: <strong class="Cor_Descrição">${data.description}</p>  </strong>
-              <p>Category:<strong class="Cor_Descrição"> ${data.category}</p> </strong>
-              
-          `;
+             <h1 class="title-detalhes">${data.title}</h1>
+         
+             <img src="${data.image}" class="Img-card" alt="Product Image">
+             <h2>Price:<span class="cor_do_preco"> $${data.price}</h2></span>
+             <p>Category:<class="Cor_Descrição"> ${data.category}</p>
+             <p>Description: <class="Cor_Descrição">${data.description}</p>
+             <p> Tipo: ${data.articleType} <br/>Ano: ${data.year} <br/>Cor base: ${data.baseColour}</p>`;
 
     const detalhesProdutoContainer =
       document.getElementById("detalhes-produto");
